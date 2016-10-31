1c8f649c5dd389a87dc5a75a290e698328b34675
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-10-31 16:30:13 +0100

    Number the list of references.

--- R/betapoisson.R
+++ R/betapoisson.R
@@ -6,10 +6,14 @@
 #'     \deqn{E(X) = E(lambda) = \frac{a}{a+b}}{E(X) = E(lambda) = a/(a+b)}
 #'     \deqn{V(X) = E(lambda) + V(lambda) = \frac{a}{a+b} + \frac{ab}{(a+b)^2(a+b+1)}}{V(X) = E(lambda) + V(lambda) = a/(a+b) + (ab)/((a+b)^2(a+b+1))}
 #'
-#' @references See pages 43 ff. in Jan Grandell, Mixed Poisson
-#'     Processes, CRC Press, 1997 and also Dimitris Karlis and Evdokia
-#'     Xekalaki, Mixed Poisson Distributions, International
-#'     Statistical Review (2005), *73*, 1, 35-58
+#' @references
+#' \enumerate{
+#'   \item Jan Grandell, Mixed Poisson Processes, CRC Press, 1997,
+#'       pages 43 ff.
+#'   \item Dimitris Karlis and Evdokia Xekalaki, Mixed Poisson
+#'       Distributions, International Statistical Review (2005), *73*,
+#'       1, 35-58
+#' }
 #'
 #' @export
 dbetapoisson <- function(x, a, b, lambda = 1) {
