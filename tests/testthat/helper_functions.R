a0d44621ea0d6c4d716e0af55ce2c44eef8cfa34
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-10-31 14:41:23 +0100

    Refactor E(X) test case.

--- tests/testthat/helper_functions.R
+++ tests/testthat/helper_functions.R
@@ -1,7 +1,11 @@
 expected_value <- function(density, a, b) {
-    find_sum(function(x) x * density(x, a = a, b = b), from = 0, to = 10)
+    find_sum(function(x) x * density(x, a, b), 0L, find_upper_limit(a, b))
 }
 
 find_sum <- function(f, from, to) {
     sum(vapply(from:to, f, double(1)))
+}
+
+find_upper_limit <- function(a, b) {
+    as.integer(qpois(-30, lambda = a/(a+b), lower.tail = FALSE, log.p = TRUE))
 }
--- tests/testthat/test-dbetapoisson.R
+++ tests/testthat/test-dbetapoisson.R
@@ -10,6 +10,6 @@ test_that("probabilities are >= 0 for values >= 0", {
     expect_gte(min(dbetapoisson(1:10, a = 2, b = 3)), 0)
 })
 
-test_that("a = 2 and b = 3 results in E(X) = 2 / (2 + 3) = .4", {
-    expect_equal(expected_value(dbetapoisson, a = 2, b = 3), .4)
+test_that("a = 2 and b = 3 results in E(X) = 2/5", {
+    expect_equal(expected_value(dbetapoisson, a = 2, b = 3), 2/5)
 })
