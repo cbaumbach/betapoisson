bfd4645ac2014abebb8365d4413fc6c41661f3e5
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-10-31 16:53:27 +0100

    Refactor test cases.

--- tests/testthat/test-dbetapoisson.R
+++ tests/testthat/test-dbetapoisson.R
@@ -14,12 +14,12 @@ test_that("probabilities are >= 0 for values >= 0", {
     expect_gte(min(dbetapoisson(1:10, a = 2, b = 3)), 0)
 })
 
-test_that("a = 2 and b = 3 results in E(X) = 2/5", {
-    expect_equal(expected_value(dbetapoisson, a = 2, b = 3), 2/5)
+test_that("a = 2 and b = 3 results in correct E(X)", {
+    expect_equal(expected_value(dbetapoisson, a = 2, b = 3), find_mean(a = 2, b = 3))
 })
 
-test_that("a = 2 and b = 3 results in V(X) = 11/25", {
-    expect_equal(variance(dbetapoisson, a = 2, b = 3), 11/25)
+test_that("a = 2 and b = 3 results in correct V(X)", {
+    expect_equal(variance(dbetapoisson, a = 2, b = 3), find_variance(a = 2, b = 3))
 })
 
 test_that("a = 2 and b = 3 results in correct skewness", {
