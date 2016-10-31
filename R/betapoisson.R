97dd50192b2776f1b42f1a370ec335d0661519e7
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-10-31 13:58:15 +0100

    Add another reference.

--- R/betapoisson.R
+++ R/betapoisson.R
@@ -1,7 +1,9 @@
 #' Probability function of Beta-Poisson mixture distribution
 #'
 #' @references See pages 43 ff. in Jan Grandell, Mixed Poisson
-#'     Processes, CRC Press, 1997
+#'     Processes, CRC Press, 1997 and also Dimitris Karlis and Evdokia
+#'     Xekalaki, Mixed Poisson Distributions, International
+#'     Statistical Review (2005), *73*, 1, 35-58
 #'
 #' @export
 dbetapoisson <- function(x, a, b) {
