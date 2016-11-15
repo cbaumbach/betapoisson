acde5992cd5c5395dff0996930b530c57fc95339
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-11-15 11:50:09 +0100

    Refactor dbetapoisson function.

--- R/betapoisson.R
+++ R/betapoisson.R
@@ -32,10 +32,8 @@
 #'
 #' @export
 dbetapoisson <- function(x, a, b, lambda = 1) {
-    missing <- is.na(x)
-    x[missing] <- 0
-    negative <- x < 0
-    x[negative] <- 0
+    x[missing <- is.na(x)] <- 0
+    x[negative <- x < 0] <- 0
     log_numerator <- x * log(lambda) + lbeta(x + a, b) + kummers_m(a + x, a + b + x, -lambda)
     log_denominator <- lfactorial(x) + lbeta(a, b)
     result <- exp(log_numerator - log_denominator)
